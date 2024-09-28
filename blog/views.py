6a42ce8656ab25ccb036dda922f9052ecac28122
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-09-28 19:28:11 -0300

    templates list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.http import Http404
 
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+    
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+    return render(
+        request,
+        'blog/post/details.html',
+        {'post': post}
+    )
+        
 # Create your views here.
+
