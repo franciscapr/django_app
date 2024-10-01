39121f51ae64814bece3dc55121d492628379e8f
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-01 16:21:24 -0300

    create model comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Comment, Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -13,3 +13,8 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
