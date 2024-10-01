2faef57a9dbf46c9c9d9522d5218b15704b108d3
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-01 17:24:19 -0300

    add comments

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from .models import Post
 from django.http import Http404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 
 def post_list(request):
@@ -33,10 +34,17 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {'post': post,
+         'comments': comments,
+         'form': form
+         }
     )
         
 # Create your views here.
@@ -88,4 +96,27 @@ def post_share(request, post_id):
             'form': form,
             'sent': sent
         }
+    )
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request, 
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form':form,
+            'comment': comment
+        },
     )
