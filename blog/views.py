96eb438d8c850d0cb35729efed59cc13c3f405bc
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-03 11:06:12 -0300

    trigs

--- blog/views.py
+++ blog/views.py
@@ -178,11 +178,12 @@ def post_search(request):
             search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity('title', query)
+                    # search=search_vector,
+                    # rank=SearchRank(search_vector, search_query)
                 )
-                .filter(rank__gte=0.3)
-                .order_by('-rank')
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
             )
 
     return render(
